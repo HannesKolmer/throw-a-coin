8add62fba61e98fa2ec43cd885e18f3dd71d515a
HannesKolmer <hannes.kolmer.2007@gmail.com>
2026-05-20 13:44:37 +0200

    Initial Commit: add a coin heads or tails game

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿string BenutzerEingabe = string.Empty;
+int Münzwurf = new Random().Next(1, 3);
+string generatedMünzwurf = "";
+string playAgain = string.Empty;
+
+while (true)
+{
+    if (Münzwurf == 1)
+    {
+        generatedMünzwurf = "k";
+    }
+    else if (Münzwurf == 2)
+    {
+        generatedMünzwurf = "z";
+    }
+    Console.WriteLine("Kopf oder Zahl? (K/Z)");
+    BenutzerEingabe = Console.ReadLine().ToLower();
+    if (BenutzerEingabe == generatedMünzwurf)
+    {
+        Console.WriteLine("du hast gewonnen!!");
+        Console.WriteLine("Möchtest du nochmal spielen? (J/N)");
+        playAgain = Console.ReadLine().ToUpper();
+        if (playAgain == "J")
+        {
+            Münzwurf = new Random().Next(1, 3);
+            continue;
+        }
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Versuche es erneut!!");
+        Münzwurf = new Random().Next(1, 3);
+    }
+}
